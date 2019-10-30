8e59c6de58b518094b1a1ece583baf3d7ce2478d
Benjamin Dos Santos <dosbenjamin@gmail.com>
2019-10-30 17:33:59 +0100

    :recycle: Refactor api module

--- src/scripts/utils/api.js
+++ src/scripts/utils/api.js
@@ -1,6 +1,8 @@
 const url = 'http://192.168.33.10/ascenscia/src/api/db/counter.php'
 
-export function api () {
-  return window.fetch(url)
-    .then(response => response.json())
+export default {
+  api () {
+    return window.fetch(url)
+      .then(response => response.json())
+  }
 }
