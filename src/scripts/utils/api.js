c540e1ab9c87c07a27196295072301b007323cd7
Benjamin Dos Santos <dosbenjamin@gmail.com>
2019-10-30 18:47:48 +0100

    :wrench: Add cors mode

--- src/scripts/utils/api.js
+++ src/scripts/utils/api.js
@@ -2,7 +2,9 @@ const url = 'http://192.168.33.10/ascenscia/src/api/db/counter.php'
 
 export default {
   api () {
-    return window.fetch(url)
+    return window.fetch(url, {
+      mode: 'cors'
+    })
       .then(response => response.json())
   }
 }
