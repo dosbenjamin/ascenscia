5d3b329ecb9095a24276962d90746b4535e10dec
Benjamin Dos Santos <dosbenjamin@gmail.com>
2019-12-07 14:40:54 +0100

    :recycle: Refactor counter

--- src/api/db/counter.php
+++ src/api/db/counter.php
@@ -1,36 +1,15 @@
 <?php
 require_once('db.php');
-require_once('../twitter/twitter_query.php');
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
 
-function updateCount ($connexion) {
-  $request = $connexion -> prepare('SELECT COUNT(*) FROM tweets');
-  $request -> execute();
-  $tweets = $request -> fetchAll();
-  return (int)$tweets[0][0];
-}
-
-function addToDB ($connexion, $id) {
-  $request = $connexion -> prepare('INSERT INTO tweets (tweet_id) VALUES (?)');
-  $request -> execute([$id]);
-}
-
-$dbCount = updateCount($connexion);
-$apiArray = $string['statuses'];
-$apiCount = count($apiArray);
-
-while ($dbCount != $apiCount || $apiArray == 1) {
-  $rCount = $apiCount - $dbCount - 1;
-  $id = $apiArray[$rCount]['id'];
-  addToDB($connexion, $id);
-  $dbCount++;
-}
-
-$dbCount = updateCount($connexion);
+$request = $connexion -> prepare('SELECT * FROM tweets');
+$request -> execute();
+$tweets = $request -> fetchAll();
+$count = (int)$tweets[0]['count'];
 
 $jsonArray = [
-  'count' => $dbCount,
-  // 'last_user' => '@'.$apiArray[0]['user']['screen_name']
+  'count' => $count,
 ];
+
 echo json_encode($jsonArray);
--- src/scripts/components/counter.js
+++ src/scripts/components/counter.js
@@ -2,7 +2,7 @@ import Odometer from 'odometer'
 
 export default {
   init (i) {
-    const $el = document.querySelector('span')
+    const $el = document.querySelector('.counter__number')
     const od = new Odometer({
       el: $el,
       format: '(,ddd)',
