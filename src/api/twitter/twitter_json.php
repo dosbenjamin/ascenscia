fd8b1a5cd84a08be8e170f9bcd873e0d46a1380e
Benjamin Dos Santos <dosbenjamin@gmail.com>
2019-12-07 14:38:13 +0100

    :wrench: Add DB query

--- src/api/twitter/twitter_json.php
+++ src/api/twitter/twitter_json.php
@@ -1,4 +1,13 @@
 <?php
 require_once('twitter_query.php');
+require_once('../db/db.php');
+
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+
+$count = count($string['statuses']);
+$request = $connexion -> prepare('UPDATE tweets SET count = :count WHERE ID = 1');
+$request -> bindValue(':count', $count);
+$request -> execute();
+
 echo json_encode($string);
